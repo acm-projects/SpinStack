0ddac670892fcd353c422b0783f704a4d49121bc
Braxton Riggs <highskies8@gmail.com>
2025-09-18 14:48:14 -0500

    Created basic expo project for env-setup

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function ProfilePage() {
+
+    return (
+        <View>
+            <Text>
+                Hello world!
+            </Text>
+        </View>
+
+    )
+}
