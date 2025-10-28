303eaf8ad31e95e1375a17dd52d2d7d9538dce95
Yugank Mishra <findyugank@gmail.com>
2025-10-28 00:16:43 -0500

    tab bar realignment + (hopefully?) vinyl fix

--- frontend/components/newMoment.tsx
+++ frontend/components/newMoment.tsx
@@ -54,6 +54,7 @@ export default function MomentView({ data }: { data: MomentInfo }) {
           useNativeDriver: true,
         })
       );
+      
       loop.start();
     } else {
       spinAnim.stopAnimation();
