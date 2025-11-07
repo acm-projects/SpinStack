bbdb1324ee3434a964a19fe4c464a695098e6f49
Yugank Mishra <findyugank@gmail.com>
2025-11-07 13:56:07 -0600

    create button merge conflict override fix

--- frontend/app/profileSettings.tsx
+++ frontend/app/profileSettings.tsx
@@ -6,7 +6,6 @@ import Feather from "react-native-vector-icons/Feather";
 import { RelativePathString, useRouter } from "expo-router";
 import Bubble from '../assets/other/bubble.svg';
 import { AutoSizeText, ResizeTextMode } from 'react-native-auto-size-text';
-import { AutoSizeText, ResizeTextMode } from 'react-native-auto-size-text';
 
 export default function ProfileSettings() {
   const { width } = Dimensions.get("window");
